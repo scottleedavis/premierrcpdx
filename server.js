6de2591643bab765613f04294a03984ab030f84f
skawtus <skawtus@gmail.com>
2014-10-25 20:19:58 -0700

    package.json cleanup.

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 __ = require('underscore');
 
-var express = require('express'),
-      races = require('./routes/race_results');
- 
+var express = require('express')
+var app = express(),
+      races = require('./routes/race_results')
 
-var app = express();
-app.use(express.bodyParser());
-
-
-app.use("/", express.static(__dirname + '/public'));
-
-//require('express-debug')(app, {/* settings */});
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/races', races.index);
 
-//app.listen(3000);
-//console.log('Listening on port 3000...');
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
