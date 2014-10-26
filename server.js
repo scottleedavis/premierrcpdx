8e38f103541b8bfaf14a77f6cb9a933852b11a01
skawtus <skawtus@gmail.com>
2014-10-25 20:15:15 -0700

    trying without specified port.

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ app.use("/", express.static(__dirname + '/public'));
 
 app.get('/races', races.index);
 
-app.listen(3000);
-console.log('Listening on port 3000...');
+//app.listen(3000);
+//console.log('Listening on port 3000...');
