366eb6c7f28018201484cda79a0e81125a37a9d4
Scott Davis <scott.davis@webtrends.com>
2014-10-31 13:43:01 -0700

    linked results file to git submodule.

--- routes/race_results.js
+++ routes/race_results.js
@@ -3,7 +3,7 @@
 YAML = require('yamljs');
 
 exports.index = function(req, res) {
-    var nativeObject = YAML.load('./routes/results_store.yml');
+    var nativeObject = YAML.load('./rctech-forum-scrapper/results_store.yml');
     res.send(nativeObject);
 };
  
