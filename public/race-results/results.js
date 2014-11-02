5be89baf92a9399744e52a38eb020e5c119d4d2d
skawtus <skawtus@gmail.com>
2014-11-01 17:21:05 -0700

    added underscore templating for race results display.

--- public/race-results/results.js
+++ public/race-results/results.js
@@ -1,5 +1,50 @@
 (function(){
     
-    console.log('results activated.');
+    console.log('results loading...');
+    
+    $ = jQuery;
+    $.getJSON( "../races/", function( data ) {
+    
+        console.log('loaded results.');
+        
+        var items = [];
+        var template = _.template(
+            $( "script.template" ).html()
+        );
+
+        var offroad = _.each(data[":offroad"], function(item){
+            item.type = "Offroad";
+        });
+        var onroad = _.each(data[":onroad"], function(item){
+            item.type = "Onroad";
+        });
+        var oval = _.each(data[":oval"], function(item){
+            item.type = "Oval";
+        });
+        
+        var races = _.union(offroad, onroad, oval);
+        races = _.sortBy(races, function(r){
+            return Object.keys(r)[0];
+        }).reverse();
+        
+        _.each( races, function(race){
+            var date = Object.keys(race)[0];
+            var type = race.type;
+            var file = race[date];
+            var filename_chunks = file.split('/');
+            var file_name = filename_chunks[filename_chunks.length-1].split('-');
+            file_name.shift()
+            file_name = file_name.join(' ');
+
+            $( "#results-list" ).append(
+                template({
+                    type: type,
+                    date: date,
+                    file: file,
+                    file_name: file_name
+                })
+            );   
+        });
+    });
     
 })();
